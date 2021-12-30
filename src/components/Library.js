6081ed33167dff5d065b0b5e7e151b09324d539b
Laurentiu Maran <laurentiu.maran01@e-uvt.ro>
2021-12-30 18:49:00 +0200

    initial commit

---
+++ src/components/Library.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Form from './Form';
+import Table from './Table';
+
+export default function Library() {
+  return (
+    <React.Fragment>
+      <Form />
+      <Table />
+    </React.Fragment>
+  );
+}
